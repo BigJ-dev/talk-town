ff581e473bbb8a69056f00c3b84e4c5c5e54e062
BigJ-dev <76684801+BigJ-dev@users.noreply.github.com>
2024-03-24 14:32:02 +0200

    first commit

---
+++ src/main/java/com/talk/town/TalkTownApplication.java
@@ -0,0 +1,13 @@
+package com.talk.town;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talk/town/TalkTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talk.town;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
